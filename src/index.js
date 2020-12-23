69fff815f98cffb560d56666b3866043e188ee18
**liu_zyong** <**liu_zyong@cvicse.com**>
2020-12-23 15:15:54 +0800

    add mobx

--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { types } from "mobx-state-tree";
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const Todo = types.model({
+  name: types.optional(types.string, ""),
+  done: types.optional(types.boolean, false)
+})
+
+const User = types.model({
+  name: types.optional(types.string, ""),
+})
+
+const RootStore = types.model({
+  users: types.map(User),
+  todos: types.optional(types.map(Todo),{})
+
+})
+
+const store = RootStore.create({
+  // users:{}
+})
+
+// const john = User.create({name:"ant"})
+// const eat = Todo.create()
+// console.log("John",john.toJSON())
+// console.log("eat",eat.toJSON())
 
 ReactDOM.render(
   <React.StrictMode>
