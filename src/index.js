cba33dff292df7517d795917886e0eb44e7ee790
liuzyong <412374299@qq.com>
2020-12-25 08:08:19 +0000

    add observer

--- src/index.js
+++ src/index.js
@@ -1,43 +1,76 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
 import { types } from "mobx-state-tree";
+import { observer } from "mobx-react";
 
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const randomId = () => Math.floor(Math.random() * 1000).toString(36);
 
-const Todo = types.model({
-  name: types.optional(types.string, ""),
-  done: types.optional(types.boolean, false)
-})
+const Todo = types
+  .model({
+    name: types.optional(types.string, ""),
+    done: types.optional(types.boolean, false)
+  })
+  .actions(self => {
+    function setName(newName) {
+      self.name = newName;
+    }
+
+    function toggle() {
+      self.done = !self.done;
+    }
+
+    return { setName, toggle };
+  });
 
 const User = types.model({
-  name: types.optional(types.string, ""),
-})
+  name: types.optional(types.string, "")
+});
 
-const RootStore = types.model({
-  users: types.map(User),
-  todos: types.optional(types.map(Todo),{})
+const RootStore = types
+  .model({
+    users: types.map(User),
+    todos: types.optional(types.map(Todo), {})
+  })
+  .actions(self => {
+    function addTodo(id, name) {
+      self.todos.set(id, Todo.create({ name }));
+    }
 
-})
+    return { addTodo };
+  });
 
 const store = RootStore.create({
-  // users:{}
-})
+  users: {},
+  todos: {
+    "1": {
+      name: "Eat a cake",
+      done: true
+    }
+  }
+});
 
-// const john = User.create({name:"ant"})
-// const eat = Todo.create()
-// console.log("John",john.toJSON())
-// console.log("eat",eat.toJSON())
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const TodoView = observer(props =>
+  <div>
+    <input
+      type="checkbox"
+      checked={props.todo.done}
+      onChange={e => props.todo.toggle()}
+    />
+    <input
+      type="text"
+      value={props.todo.name}
+      onChange={e => props.todo.setName(e.target.value)}
+    />
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const AppView = observer(props =>
+  <div>
+    <button onClick={e => props.store.addTodo(randomId(), "New Task")}>
+      Add Task
+    </button>
+   {props.store.todos.values().map(todo => <TodoView todo={todo} />)}
+  </div>
+);
+
+render(<AppView store={store} />, document.getElementById("root"));
